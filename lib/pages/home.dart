8749cf2c20493c6d369ea6206bf90a5c461bf358
Suhas Thalanki <suhastvs@gmail.com>
2021-06-14 13:00:27 +0530

    basic layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("VaxApp"),
+        title: Text(
+            "VaxApp",
+            style: TextStyle(
+              color: Colors.amberAccent[200],
+            ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.grey[800],
+      ),
+      backgroundColor: Colors.grey[850],
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 50,),
+          Container(
+            child: Center(
+              child: Icon(
+                  Icons.medication,
+                  size: 150,
+                  color: Colors.amberAccent[200],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
