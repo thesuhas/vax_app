797d58a74204e7e89429de7753a99322fab30f8e
Suhas Thalanki <suhastvs@gmail.com>
2021-06-20 00:37:33 +0530

    added status to show whether booking is going on

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,10 @@ import 'package:vax_app/widgets/bencard.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
+
+  bool? _booking;
+  String? _button = "Book";
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -16,6 +20,48 @@ class _HomeState extends State<Home> {
   void _redirect() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('redirect', true);
+  }
+
+  void _checkBooking() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    widget._booking = prefs.getBool('booking');
+    if (widget._booking == null || widget._booking == false) {
+      widget._booking = false;
+    }
+    else if (widget._booking == true) {
+      setState(() {
+        widget._button = "Booking";
+      });
+    }
+  }
+
+  void _startBooking() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('booking', true);
+    setState(() {
+      widget._booking = true;
+    });
+  }
+
+  void _endBooking() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('booking', false);
+    setState(() {
+      widget._booking = false;
+    });
+  }
+
+  bool _checkBool(bool? test) {
+    if (test == null || test == false) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkBooking();
   }
 
   @override
@@ -99,6 +145,21 @@ class _HomeState extends State<Home> {
       body: Container(
         child: ListView(
           children: <Widget>[
+            Center(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, !_checkBool(widget._booking) ? 0 : 20, 10, 10),
+                child: !_checkBool(widget._booking)  ?
+                    SizedBox(height: 0, width: 0,):
+                Text(
+                  "Booking in Progress",
+                  style: TextStyle(
+                    color: Colors.red[900],
+                    fontSize: 20,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+            ),
             BenCard(name: "Suhas Thalanki", benID: "123456789", vaccineStatus: "Partially Vaccinated", vaccine: "COVAXIN",),
             BenCard(name: "Roopak Maddara", benID: "6969696969", vaccineStatus: "Not Vaccinated",),
             BenCard(name: "Sachin Shankar", benID: "4204204202", vaccineStatus: "Fully Vaccinated", vaccine: "BALLER",),
@@ -106,13 +167,33 @@ class _HomeState extends State<Home> {
             SizedBox(height: 20,),
             Container(
               margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-              width: 50,
+              width: !_checkBool(widget._booking) ? 50 : 100,
               child: Center(
-                child: TextButton(
-                  onPressed: () {},
+                child: !_checkBool(widget._booking) ? TextButton(
+                  onPressed: () {
+                    if (!_checkBool(widget._booking)) {
+                      _startBooking();
+                    }
+                  },
                   child: Text(
                     "Book"
                     ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amberAccent[200],
+                    primary: Colors.black,
+                    textStyle: TextStyle(
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ) : TextButton(
+                  onPressed: () {
+                    if (_checkBool(widget._booking)) {
+                      _endBooking();
+                    }
+                  },
+                  child: Text(
+                      "End Booking"
+                  ),
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.amberAccent[200],
                     primary: Colors.black,
